2a5cb2339fbf13473eab9e681e8c9a97aa8bfdad
flx <lixing-feng@outlook.com>
2026-02-04 15:19:37 +0800

    add IDE hint

--- include/kSimd_extension/vmath.hpp
+++ include/kSimd_extension/vmath.hpp
@@ -10,9 +10,11 @@
 
 #define KSIMD_API(ret) KSIMD_DYN_FUNC_ATTR KSIMD_FORCE_INLINE KSIMD_FLATTEN ret KSIMD_CALL_CONV
 
-// 由于模板参数op没有代码提示，所以给IDE一个默认值提示，之后一定要注释掉
+// 由于模板参数op没有代码提示，所以给IDE一个默认值提示
 #ifdef KSIMD_IDE
-    // #define op BaseOp<SimdInstruction::AVX, float32>
+    #define KSIMD_IDE_HINT using op = BaseOp<SimdInstruction::AVX, float32>;
+#else
+    #define KSIMD_IDE_HINT
 #endif
 
 namespace KSIMD_NAMESPACE_NAME::ext::KSIMD_DYN_INSTRUCTION::vmath
@@ -22,6 +24,7 @@ namespace KSIMD_NAMESPACE_NAME::ext::KSIMD_DYN_INSTRUCTION::vmath
     template<typename op, is_batch_type batch_t>
     KSIMD_API(batch_t) clamp(batch_t v, batch_t min, batch_t max) noexcept
     {
+        KSIMD_IDE_HINT
         return op::min(max, op::max(v, min));
     }
 
@@ -33,6 +36,7 @@ namespace KSIMD_NAMESPACE_NAME::ext::KSIMD_DYN_INSTRUCTION::vmath
     template<typename op, is_batch_type_includes<float32, float64> batch_t>
     KSIMD_API(batch_t) lerp(batch_t a, batch_t b, batch_t t) noexcept
     {
+        KSIMD_IDE_HINT
         // result = (b - a) * t + a
         return op::mul_add(op::sub(b, a), t, a);
     }
@@ -40,6 +44,8 @@ namespace KSIMD_NAMESPACE_NAME::ext::KSIMD_DYN_INSTRUCTION::vmath
     template<typename op, is_batch_type_includes<float32, float64> batch_t>
     KSIMD_API(batch_t) sin(batch_t v) noexcept
     {
+        KSIMD_IDE_HINT
+
         constexpr SimdInstruction Instruction = op::internal_instruction_;
         constexpr size_t Lanes = op::Lanes;
 
