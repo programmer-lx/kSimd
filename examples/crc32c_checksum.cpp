b14068ef932ebed038fec5cc3879b4fc31819032
flx <lixing-feng@outlook.com>
2026-02-14 22:52:18 +0800

    Update crc32c_checksum.cpp

--- examples/crc32c_checksum.cpp
+++ examples/crc32c_checksum.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-    constexpr size_t NUM = 30 * 1024 * 1024 + 3; // 30MB + 3B (unaligned)
+    constexpr size_t NUM = 100 * 1024 * 1024 + 3; // 100MB + 3B (unaligned)
     std::vector<uint8_t> buffer(NUM);
     for (size_t i = 0; i < NUM; ++i)
     {
@@ -22,10 +22,28 @@ int main()
     // 计时
     {
         ScopeTimer timer("crc32c");
-        checksum = ks_update_crc32c(ks_begin_crc32c(), buffer.data() + 3, NUM - 3);
+        checksum = ks_begin_crc32c();
+        checksum = ks_update_crc32c(checksum, buffer.data() + 3, NUM - 3);
         checksum = ks_end_crc32c(checksum);
 
         std::cout << std::format("checksum: {}", checksum) << std::endl;
+    }
+
+    // 验证已知的CRC32C
+    {
+        checksum = ks_begin_crc32c();
+        const char* str = "123456789";
+        checksum = ks_update_crc32c(checksum, reinterpret_cast<const uint8_t*>(str), 9);
+        checksum = ks_end_crc32c(checksum);
+
+        if (checksum == UINT32_C(0xE3069283))
+        {
+            std::cout << "123456789 CRC32C = 0xE3069283, correct!\n";
+        }
+        else
+        {
+            std::cout << "Incorrect CRC32C checksum!\n";
+        }
     }
 
     return 0;
